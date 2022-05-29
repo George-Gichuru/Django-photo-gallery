7d10021b22eb380f695d843b57525f8c5c004e8e
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-05-29 19:22:15 +0300

    Added model to the image view page

--- images/admin.py
+++ images/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Image, Location, Category
 
-# create admin page here
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['name','category','location','pub_date']
